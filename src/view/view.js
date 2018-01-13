d943aa95b41a6c4e87a75322394f4c1225c64c1f
AlexeyCheremnykh <chmnkh@gmail.com>
2018-01-14 00:08:18 +0700

    Тесты для сетки - ок; сетка рисуется ок

--- src/view/view.js
+++ src/view/view.js
@@ -1,54 +1,54 @@
 class View {
     constructor() {}
     
-    // ширина и высота - в клетках
-    // размер клетки - для расчета ширины и высоты в пикселях
-    createCanvas(width, height, cellSize) {
-        if (width < 1 || height < 1 || !Number.isInteger(width) || !Number.isInteger(height)) {
+    createCanvas(width, height) {
+        if (width < 1 || height < 1 || isNaN(width) || isNaN(height)) {
             throw new Error("Wrong input");
         }
 
         let canvas = document.createElement("canvas");
-        canvas.width = width * cellSize;
-        canvas.height = height * cellSize;
-        canvas.className = ".game__grid";
+        canvas.width = width;
+        canvas.height = height;
+        canvas.className = "game__grid";
+        canvas.id = "game__grid";
 
         return canvas;
     }    
     
 
-    drawGrid(canvas) {
+    drawGrid(canvas, cellsX, cellsY) {
         try {
-            var ctx = canvas.getContext("2d");
-        } catch (err) {
-            return
+            canvas = new fabric.Canvas(canvas.id);
+        } catch(err) {
+            return new fabric.Canvas();
         }
 
-        ctx.lineWidth = 1;
-
-        let cellSize = 10;
-
-        canvas = new fabric.Canvas(canvas);        
-
-        let rects = [];
-        for (let x = 0; x < 10; x++) {
-            for (let y = 0; y < 10; y++) {
-                let rect = new fabric.Rect({
-                    left: x,
-                    top: y,
-                    width: 10,
-                    height: 10,
+        const cellSize = 10;
+        for (let x = 0; x < cellsX; x++) {
+            for (let y = 0; y < cellsY; y++) {
+                let rect = new fabric.Rect({                    
+                    width: cellSize,
+                    height: cellSize,
+                    left: x * cellSize,
+                    top: y * cellSize,
                     strokeWidth: 1,
-                    stroke: "#000"
-                });                
-                canvas.add(rect);
-                rects.push(rect);                
+                    stroke: "#000",
+                    fill: "transparent",
+                    selectable: false
+                }); 
+                console.log("x: ", x, " y: ", y);         
+                canvas.add(rect);           
             }                      
         }
 
-        console.log(rects[0] instanceof fabric.Rect);
-        return 0;
+        return canvas;
     }
 }
+
+// testing
+const view = new View();
+const canvas = view.createCanvas(200, 200);
+document.body.querySelector(".game__grid-container").appendChild(canvas);
+view.drawGrid(canvas, 8, 8);
 
 module.exports = View;
