146141618d44fe70408a9717ead1947d812827dd
AlexeyCheremnykh <chmnkh@gmail.com>
2018-03-11 16:23:13 +0700

    controller.test.js дописал

--- src/blocks/game/controller/controller.test.js
+++ src/blocks/game/controller/controller.test.js
@@ -1,185 +1,145 @@
-/* global test, describe, jest, expect */
-
 import Model from '../model/model';
 import View from '../view/view';
 import Controller from './controller';
 
+const elements = [
+  '<div class="js-game__grid"></div>',
+  '<button class="js-game__start-stop"/>',
+  '<button class="js-game__one-step"/>',
+  '<button class="js-game__clear"/>',
+  '<input class="js-game__delay"/>',
+  '<input class="js-game__width" value="30"/>',
+  '<input class="js-game__height" value="20"/>',
+];
+const $body = $('body');
+$body.append([...elements]);
+
+
 const model = new Model();
-const view = new View(model);
+const view = new View();
 const controller = new Controller(model, view);
-const grid = document.createElement('div');
-grid.className = 'game__grid js-game__grid';
-document.body.appendChild(grid);
 
-const startStopBtn = document.createElement('button');
-startStopBtn.className = 'game__start-stop js-game__start-stop';
-startStopBtn.innerHTML = 'Start';
-document.body.appendChild(startStopBtn);
+const observeView = jest.spyOn(controller, 'observeView');
+const observeModel = jest.spyOn(controller, 'observeModel');
+const createGridMatrix = jest.spyOn(model, 'createGridMatrix');
+const startGame = jest.spyOn(controller, 'startGame');
+const updateViewCell = jest.spyOn(view.grid, 'updateCell');
+const updateModelCell = jest.spyOn(model, 'updateCell');
 
-const clearBtn = document.createElement('button');
-clearBtn.className = 'game__clear js-game__clear';
-document.body.appendChild(clearBtn);
-
-const oneStepBtn = document.createElement('button');
-oneStepBtn.className = 'game__one-step js-game__one-step';
-document.body.appendChild(oneStepBtn);
-
-const widthInput = document.createElement('input');
-widthInput.className = 'game__width-input js-game__width-input';
-document.body.appendChild(widthInput);
-
-const heightInput = document.createElement('input');
-heightInput.className = 'game__height-input js-game__height-input';
-document.body.appendChild(heightInput);
-
-const delayInput = document.createElement('input');
-delayInput.className = 'game__delay-input js-game__delay-input';
-delayInput.value = 100;
-document.body.appendChild(delayInput);
-
-const createGridMatrixSpy = jest.spyOn(model, 'createGridMatrix');
-const calculateNextGenerationSpy = jest.spyOn(model, 'calculateNextGeneration');
-const updateCellSpy = jest.spyOn(model, 'updateCell');
-const replaceStartButtonSpy = jest.spyOn(view, 'replaceStartButton');
-const replaceStopButtonSpy = jest.spyOn(view, 'replaceStopButton');
-
-controller.observeModel();
-model.createGridMatrix(5, 5);
-controller.setListeners();
 
 describe('Controller tests', () => {
-  test('Class instance has been created', () => {
-    expect(controller._model).not.toBeUndefined();
-    expect(controller._view).not.toBeUndefined();
+  jest.useFakeTimers();
+
+  describe('Initiation', () => {
+    test('Class instance has been created correctly', () => {
+      expect(controller._model).not.toBeUndefined();
+      expect(controller._view).not.toBeUndefined();
+    });
+
+    test('Controller has been initiated', () => {
+      controller.init();
+      expect(observeModel).toHaveBeenCalled();
+      expect(observeView).toHaveBeenCalled();
+      expect(createGridMatrix).toHaveBeenCalled();
+      expect(controller._timerId).toBe(null);
+      expect(controller._gameIsRunning).toBe(false);
+      expect(controller._delay).not.toBeUndefined();
+    });
   });
 
-  describe('Events listening', () => {
-    jest.useFakeTimers();
-
-    test('Cell update on click', () => {
-      const $gridCell = $($('.js-game__grid-cell')[2]);
-      $gridCell.trigger('mousedown');
-      expect(updateCellSpy).toHaveBeenCalledWith(0, 2);
+  describe('Start and stop game', () => {
+    test('Game has been started if valid delay', () => {
+      controller.startGame();
+      expect(controller._timerId).not.toBe(null);
+      expect(controller._gameIsRunning).toBe(true);
     });
 
-    describe('Button click events', () => {
-      const $startStopBtn = $('.js-game__start-stop');
-
-      test('Game starts on click', () => {
-        $startStopBtn.trigger('click');
-        expect(setInterval).toHaveBeenCalled();
-        expect(replaceStartButtonSpy).toHaveBeenCalled();
-      });
-
-      test('Game stops on click', () => {
-        $startStopBtn.trigger('click');
-        expect(clearInterval).toHaveBeenCalled();
-        expect(replaceStopButtonSpy).toHaveBeenCalled();
-      });
-
-      test('Calculate one step next generation on click', () => {
-        const $oneStepBtn = $('.js-game__one-step');
-        $oneStepBtn.trigger('click');
-        expect(calculateNextGenerationSpy).toHaveBeenCalled();
-      });
-
-      test('Grid clears on click', () => {
-        const $clear = $('.js-game__clear');
-        $clear.trigger('click');
-        expect(createGridMatrixSpy).toHaveBeenCalledWith(5, 5);
-      });
+    test('Game has been stopped', () => {
+      controller.stopGame();
+      expect(clearInterval).toHaveBeenCalled();
+      expect(controller._gameIsRunning).toBe(false);
     });
 
-    describe('Grid size change events', () => {
-      const $width = $('.js-game__width-input');
-      const $height = $('.js-game__height-input');
-      $width.val(4);
-      $height.val(12);
+    test('Game hasn`t been started if invalid delay', () => {
+      controller._view.delay.addInvalidModificator();
+      controller.startGame();
+      expect(controller._gameIsRunning).toBe(false);
+      controller._view.delay.removeInvalidModificator();
+    });
+  });
 
-      test('New grid if width changes', () => {
-        $width.trigger('focus');
-        $width.val(5);
-        $width.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenLastCalledWith(5, 12);
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(3);
-      });
-
-      test("The same grid if width doesn't change", () => {
-        $width.trigger('focus');
-        $width.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(3);
-      });
-
-      test('New grid if height changes', () => {
-        $height.trigger('focus');
-        $height.val(6);
-        $height.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenLastCalledWith(5, 6);
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(4);
-      });
-
-      test("The same grid if height doesn't change", () => {
-        $height.trigger('focus');
-        $height.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(4);
-      });
-
-      test('The same grid if width is invalid', () => {
-        $width.val('wrong!');
-        $width.trigger('focus');
-        $width.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(4);
-
-        $width.val('-124');
-        $width.trigger('focus');
-        $width.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(4);
-      });
-
-      test('The same grid if height is invalid', () => {
-        $width.val(5);
-        $height.val('wrong!');
-        $height.trigger('focus');
-        $height.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(4);
-
-        $height.val('-666');
-        $height.trigger('focus');
-        $height.trigger('blur');
-        expect(createGridMatrixSpy).toHaveBeenCalledTimes(4);
-      });
+  describe('Cell updating', () => {
+    test('Correct grid cell has been updated', () => {
+      controller.updateGridCell(1, 1);
+      expect(updateViewCell).toHaveBeenCalledWith(31);
+      controller.updateGridCell(2, 1);
+      expect(updateViewCell).toHaveBeenCalledWith(61);
+      controller.updateGridCell(3, 0);
+      expect(updateViewCell).toHaveBeenCalledWith(90);
     });
 
-    describe('Delay change events', () => {
-      const $delay = $('.js-game__delay-input');
-      const $startStopBtn = $('.js-game__start-stop');
-      $startStopBtn.trigger('click');
+    test('Correct matrix cell has been updated', () => {
+      controller.updateMatrixCell(31);
+      expect(updateModelCell).toHaveBeenCalledWith(1, 1);
+      controller.updateMatrixCell(61);
+      expect(updateModelCell).toHaveBeenCalledWith(2, 1);
+      controller.updateMatrixCell(90);
+      expect(updateModelCell).toHaveBeenCalledWith(3, 0);
+    });
+  });
 
-      test('New timer if delay changes', () => {
-        $delay.trigger('focus');
-        $delay.val(200);
-        $delay.trigger('blur');
-        expect(clearInterval).toHaveBeenCalledTimes(2);
-        expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 200);
-      });
+  describe('Matrix size changing', () => {
+    test('Matrix width has been changed if valid', () => {
+      controller.changeMatrixWidth('5');
+      expect(createGridMatrix).toHaveBeenCalledWith(5, 20);
+    });
 
-      test("The same timer if delay doesn't change", () => {
-        $delay.trigger('focus');
-        $delay.trigger('blur');
-        expect(clearInterval).toHaveBeenCalledTimes(2);
-      });
+    test('Matrix width hasn`t been changed if invalid', () => {
+      controller.changeMatrixWidth('abc');
+      controller.changeMatrixWidth('0.5');
+      controller.changeMatrixWidth('12ss');
+      expect(createGridMatrix).toHaveBeenCalledTimes(2);
 
-      test('The same timer if delay is invalid', () => {
-        $delay.val('wrong!');
-        $delay.trigger('focus');
-        $delay.trigger('blur');
-        expect(clearInterval).toHaveBeenCalledTimes(2);
+      view.width.removeInvalidModificator();
+    });
 
-        $delay.val('-999');
-        $delay.trigger('focus');
-        $delay.trigger('blur');
-        expect(clearInterval).toHaveBeenCalledTimes(2);
-      });
+    test('Matrix height has been changed if valid', () => {
+      controller.changeMatrixHeight('8');
+      expect(createGridMatrix).toHaveBeenCalledWith(30, 8);
+    });
+
+    test('Matrix height hasn`t been changed if invalid', () => {
+      controller.changeMatrixHeight('abc');
+      controller.changeMatrixHeight('0.5');
+      controller.changeMatrixHeight('12ss');
+      expect(createGridMatrix).toHaveBeenCalledTimes(3);
+
+      view.height.removeInvalidModificator();
+    });
+  });
+
+  describe('Delay changing', () => {
+    let oldDelay = controller._delay;
+
+    test('Delay has been changed if valid', () => {
+      controller._gameIsRunning = true;
+      expect(clearInterval).toHaveBeenCalledTimes(1);
+      expect(setInterval).toHaveBeenCalledTimes(1);
+
+      controller.changeDelay('1000');
+      expect(oldDelay).not.toBe(controller._delay);
+      expect(clearInterval).toHaveBeenCalledTimes(2);
+      expect(setInterval).toHaveBeenCalledTimes(2);
+    });
+
+    test('Delay hasn`t been changed if invalid', () => {
+      oldDelay = controller._delay;
+      controller.changeDelay('abc');
+      controller.changeDelay('-5');
+      controller.changeDelay('12sss');
+      expect(oldDelay).toBe(controller._delay);
+      expect(clearInterval).toHaveBeenCalledTimes(2);
+      expect(setInterval).toHaveBeenCalledTimes(2);
     });
   });
 });
